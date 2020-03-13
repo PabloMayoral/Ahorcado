4c20bb9815fa6ec7879e5c94365b11f9dddb08bb
PabloMayoral <p.martinmayoral@gmail.com>
2020-03-13 12:55:27 +0100

    ahorcado terminado

--- app/src/main/java/com/example/ahorcado/MainActivity.java
+++ app/src/main/java/com/example/ahorcado/MainActivity.java
@@ -12,76 +12,89 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-int numeroFallos =0;
-    String palabraOculta = eligePalabra();
+    String palabraOculta = escogePalabra();
+    int numeroDeFallos = 0;
+    boolean partidaTerminada = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(savedInstanceState == null){
+        if (savedInstanceState == null){
             getSupportFragmentManager().beginTransaction().add(R.id.ventanaJuego, new VentanaAhorcado()).commit();
         }
-
-        String auxiliar = "";
-        for (int i = 0; i < palabraOculta.length(); i++) {
-            auxiliar = auxiliar + "_ ";
-        }
-        TextView textoConGuiones =  ((TextView) findViewById(R.id.palabraConGuiones));
     }
-public void botonPulsado(View vista){
-    Button boton = findViewById(vista.getId());
-    boton.setVisibility(View.INVISIBLE);
-    chequeaLetra(boton.getText().toString());
-}
-
-private void chequeaLetra(String letra){
-        letra = letra.toUpperCase();
-    ImageView imagenAhorcado = ((ImageView) findViewById(R.id.imagenAhorcado));
-    TextView textoConGuiones =  ((TextView) findViewById(R.id.palabraConGuiones));
-    String palabrasConGuiones = textoConGuiones.getText().toString();
-
-    boolean partidaTerminada = false;
-    if (palabraOculta.contains(letra)) {
-        char letraPulsada = letra.charAt(0);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String barras = "";
         for (int i = 0; i < palabraOculta.length(); i++) {
-            if (palabraOculta.charAt(i) == letra.charAt(0)) {
-                palabrasConGuiones = palabrasConGuiones.substring(0, 2 * i) + letra + palabrasConGuiones.substring(2 * i + 1);
+            barras += "_ ";
+        }
+        ((TextView) findViewById(R.id.palabraConGuiones)).setText(barras);
+    }
 
+    private String escogePalabra() {
+        String[] listaPalabras = {"Fabio", "marica", "weon","coches","lloreria","bermejogay","msi","cancelado","fraxito","puebleo",
+        "pokemongo","rodrigo","cars3","android","Ramiro"};
+        Random aleatorio = new Random();
+        int pos = aleatorio.nextInt(listaPalabras.length);
+        return listaPalabras[pos].toUpperCase();
+    }
+
+    public void botonPulsado(View vista){
+        if (!partidaTerminada) {
+            Button boton = findViewById(vista.getId());
+            boton.setVisibility(View.INVISIBLE);
+            chequeaLetra(boton.getText().toString());
+        }
+
+    }
+
+    private void chequeaLetra(String letra){
+        letra = letra.toUpperCase();
+        ImageView imagenAhorcado = ((ImageView) findViewById(R.id.imagenAhorcado));
+        TextView textoConGuiones = ((TextView) findViewById(R.id.palabraConGuiones));
+        String palabraConGuiones = textoConGuiones.getText().toString();
+
+        boolean acierto = false;
+
+        for (int i=0; i<palabraOculta.length(); i++){
+            if (palabraOculta.charAt(i) == letra.charAt(0)){
+                //quita el guión bajo de la letra correspondiente
+                palabraConGuiones = palabraConGuiones.substring(0, 2*i)
+                        + letra
+                        + palabraConGuiones.substring(2*i+1);
+                acierto = true;
             }
         }
-        textoConGuiones.setText(palabrasConGuiones);
-        if (!palabrasConGuiones.contains("_")) {
+        if (!palabraConGuiones.contains("_")){
+            partidaTerminada=true;
             imagenAhorcado.setImageResource(R.drawable.acertastetodo);
-            partidaTerminada = true;
-        }
-    }else {
-        numeroFallos++;
-        if (numeroFallos == 6) {
-            partidaTerminada = true;
-        }
-        }
-
-    if(!partidaTerminada){
-        numeroFallos++;
-        switch (numeroFallos){
-            case 0:imagenAhorcado.setImageResource(R.drawable.ahorcado_0); break;
-            case 1:imagenAhorcado.setImageResource(R.drawable.ahorcado_1); break;
-            case 2:imagenAhorcado.setImageResource(R.drawable.ahorcado_2); break;
-            case 3:imagenAhorcado.setImageResource(R.drawable.ahorcado_3); break;
-            case 4:imagenAhorcado.setImageResource(R.drawable.ahorcado_4); break;
-            case 5:imagenAhorcado.setImageResource(R.drawable.ahorcado_5); break;
-            default:imagenAhorcado.setImageResource(R.drawable.ahorcado_fin); break;
 
         }
-    }
-}
-    private String eligePalabra() {
-        String[] listaPalabras = {"HOLA", "VLADIKAKA", "BORREGUITO", "BABYYODA"};
-        Random aleatorio = new Random();
-//variable aleatoria para elegir una palabra al azar
-        int posicion = aleatorio.nextInt(listaPalabras.length);
-        return listaPalabras[posicion].toUpperCase();
+
+        textoConGuiones.setText(palabraConGuiones);
+
+        if (!acierto){
+            numeroDeFallos++;
+            switch (numeroDeFallos){
+                case 0 : imagenAhorcado.setImageResource(R.drawable.ahorcado_0); break;
+                case 1 : imagenAhorcado.setImageResource(R.drawable.ahorcado_1); break;
+                case 2 : imagenAhorcado.setImageResource(R.drawable.ahorcado_2); break;
+                case 3 : imagenAhorcado.setImageResource(R.drawable.ahorcado_3); break;
+                case 4 : imagenAhorcado.setImageResource(R.drawable.ahorcado_4); break;
+                case 5 : imagenAhorcado.setImageResource(R.drawable.ahorcado_5); break;
+                default: imagenAhorcado.setImageResource(R.drawable.ahorcado_fin); break;
+            }
+            if(numeroDeFallos==6) {
+                partidaTerminada=true;
+            imagenAhorcado.setImageResource(R.drawable.ahorcado_fin);
+            }
+        }
+
 
     }
 }
+
